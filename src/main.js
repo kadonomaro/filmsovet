b36cf117d317f85fea836bb14596dc711e66f5e5
Kadonomaro <denis.afer@gmail.com>
2020-10-09 23:53:33 +0700

    feat: add firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+
+const firebaseDevConfig = {
+	apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+	authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
+firebase.initializeApp(process.env.NODE_ENV === 'production' ? firebaseProdConfig : firebaseDevConfig);
+const database = firebase.database();
+export { database };
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
