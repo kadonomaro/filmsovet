4875ba1f16dbf72f086958a14aed31aa83005961
Kadonomaro <denis.afer@gmail.com>
2020-10-10 14:38:18 +0700

    feat: add modal component

--- src/components/Toolbar/ToolbarComponent.vue
+++ src/components/Toolbar/ToolbarComponent.vue
@@ -1,32 +1,66 @@
 <template>
 	<div class="toolbar">
-		<select class="input toolbar__select" @change="changeHandler">
-			<option value="all">Выберите жанр</option>
-			<option
-				v-for="tag in getFilmsTags"
-				:key="tag"
-				:value="tag"
-			>{{ tag }}</option>
-		</select>
-		<button
-			class="button button---icon-plus button--auto-height toolbar__button"
-			title="Добавить фильм"
-			@click="clickHandler"
-		></button>
+		<div class="toolbar__inner">
+			<select class="input toolbar__select" @change="changeHandler">
+				<option value="all">Выберите жанр</option>
+				<option
+					v-for="tag in getFilmsTags"
+					:key="tag"
+					:value="tag"
+				>{{ tag }}</option>
+			</select>
+			<button
+				class="button button---icon-plus button--auto-height toolbar__button"
+				title="Добавить фильм"
+				@click="clickHandler"
+			></button>
+		</div>
+
+		<AppModal v-show="modal.visible" @close="closeModalHandler">
+      <template v-slot:header>
+				<span>Добавить новый фильм</span>
+      </template>
+      <template v-slot:body>
+				Текст
+      </template>
+			<template v-slot:footer>
+				<button class="button button---icon-check" style="margin-right: 5px" title="Добавить"></button>
+				<button
+					class="button button--danger button---icon-cancel"
+					title="Отмена"
+					@click="closeModalHandler"
+				></button>
+      </template>
+    </AppModal>
+
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import AppModal from '../partials/AppModal';
 
 export default {
 	name: 'ToolbarComponent',
+	components: {
+		AppModal
+	},
+	data() {
+		return {
+			modal: {
+				visible: false
+			}
+		}
+	},
 	methods: {
 		changeHandler(event) {
 			this.$store.dispatch('changeFilmType', event.target.value);
 		},
 		clickHandler() {
-			console.log('click');
+			this.modal.visible = true;
+		},
+		closeModalHandler() {
+			this.modal.visible = false;
 		}
 	},
 	computed: {
@@ -37,8 +71,10 @@ export default {
 
 <style lang="scss">
 	.toolbar {
-		display: flex;
 		padding: 10px 0;
+		&__inner {
+			display: flex;
+		}
 		&__select {
 			margin-right: 10px;
 		}
---
+++ src/components/partials/AppModal.vue
@@ -0,0 +1,104 @@
+<template>
+	  <transition name="modal-fade">
+    <div class="modal-overlay" @click.self="closeHandler">
+      <div class="modal">
+        <header class="modal__header">
+          <slot name="header"></slot>
+					<button class="modal__button-close" aria-label="close modal" @click="closeHandler">
+						<svg class="modal__close-icon" fill="#a0a0a0" width="100%" height="100%" viewBox="0 0 18 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<path d="M10.415 9L15 13.584 13.587 15 9 10.415 4.413 15 3 13.584 7.585 9 3 4.416 4.413 3 9 7.585 13.587 3 15 4.416 10.415 9z"></path>
+						</svg>
+    			</button>
+        </header>
+        <div class="modal__body">
+          <slot name="body"></slot>
+        </div>
+        <footer class="modal__footer">
+          <slot name="footer"></slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+
+export default {
+  name: 'AppModal',
+  methods: {
+    closeHandler() {
+      this.$emit('close');
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+  .modal-overlay {
+		position: fixed;
+		z-index: 9999;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+		align-items: center;
+		padding: 10px;
+    background-color: rgba($color: #000000, $alpha: 0.7);
+  }
+  .modal {
+		position: relative;
+		z-index: 999;
+    min-width: 300px;
+		max-width: 600px;
+		color: #303030;
+		background-color: #ffffff;
+    &__header,
+    &__footer {
+      display: flex;
+      padding: 10px 10px;
+    }
+    &__header {
+			position: relative;
+			padding-right: 40px;
+      justify-content: space-between;
+			font-size: 20px;
+    }
+    &__body {
+      padding: 10px 10px;
+      font-size: 16px;
+    }
+    &__footer {
+			justify-content: flex-end;
+			align-items: center;
+			background-color: $color-light-gray;
+    }
+    &__button-close {
+			position: absolute;
+			top: 8px;
+			right: 5px;
+			width: 30px;
+			height: 30px;
+			padding: 0;
+			background-color: transparent;
+			border: none;
+			cursor: pointer;
+			&:hover .modal__close-icon,
+			&:focus .modal__close-icon {
+				fill: $color-accent;
+			}
+		}
+		&__close-icon {
+			transition: fill 0.1s ease-in;
+		}
+  }
+  .modal-fade-enter,
+  .modal-fade-leave-active {
+    opacity: 0;
+  }
+  .modal-fade-enter-active,
+  .modal-fade-leave-active {
+    transition: opacity 0.3s ease
+  }
+</style>
