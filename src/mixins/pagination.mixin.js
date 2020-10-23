42284f35c4c17c5649e9eec29cc4d61acc682d96
Kadonomaro <denis.afer@gmail.com>
2020-10-24 00:52:14 +0700

    feat: add pagination mixin, include on home page

---
+++ src/mixins/pagination.mixin.js
@@ -0,0 +1,32 @@
+export default {
+  data() {
+    return {
+      page: +this.$route.query.page || 1,
+      pageSize: 12,
+      pageRange: 1,
+      pageCount: 0,
+      allItems: [],
+      items: []
+    };
+  },
+  methods: {
+    pageChangeHandler(page) {
+      this.$router.push(`${this.$route.path}?page=${page}`);
+      this.items = this.allItems[page - 1] || this.allItems[0];
+    },
+    setupPagination(allItems) {
+      this.allItems = chunk(allItems, this.pageSize);
+      this.pageCount = this.allItems.length;
+      this.items = this.allItems[this.page - 1] || this.allItems[0];
+    }
+  }
+};
+
+
+function chunk(array, size) {
+	const temp = [];
+  for (let i = 0; i < array.length; i+=size) {
+    temp.push(array.slice(i, i + size));
+  }
+  return temp;
+}
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,8 +2,18 @@
   <div class="page">
     <h1 class="page__title">Советую посмотреть</h1>
 		<ToolbarComponent />
-		<FilmList :list="getFilms" v-if="getFilms.length"/>
+		<FilmList :list="items" v-if="getFilms.length"/>
 		<FilmEmpty v-else/>
+		<paginate
+      v-if="getFilms.length > pageSize"
+      v-model="page"
+      :page-count="pageCount"
+      :page-range="pageRange"
+      :click-handler="pageChangeHandler"
+      :prev-text="'<'"
+      :next-text="'>'"
+      :container-class="'paginate'">
+    </paginate>
   </div>
 </template>
 
@@ -12,16 +22,26 @@ import { mapGetters } from 'vuex';
 import FilmList from '@/components/Film/FilmList';
 import FilmEmpty from '@/components/Film/FilmEmpty';
 import ToolbarComponent from '@/components/Toolbar/ToolbarComponent';
+import paginationMixin from '@/mixins/pagination.mixin';
 
 export default {
 	name: 'HomePage',
+	mixins: [paginationMixin],
 	components: {
 		FilmList,
 		FilmEmpty,
 		ToolbarComponent
 	},
+	mounted() {
+		this.setupPagination(this.getFilms);
+	},
 	computed: {
 		...mapGetters(['getFilms'])
+	},
+	watch: {
+		getFilms(films) {
+			this.setupPagination(films);
+		}
 	},
 	metaInfo() {
 		return {
